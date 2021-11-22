1bb14f47dc06139374057e0096971be574525063
jkgamingc <80155461+jkgamingc@users.noreply.github.com>
2021-11-22 02:46:39 -0800

    Add files via upload

---
+++ source/lynx.bat
@@ -0,0 +1,33 @@
+@echo off
+if "%1" == "" (
+	echo you can download any library for Nx with lynx
+	echo --help has to be the first parameter, shows all the options
+	echo --list has to be the first parameter, shows the name of all the libraries and frameworks
+	echo --download has to be the first parameter, downloads the library or framewrok, you have to write the name of the library or framework in the second parameter
+)
+
+if "%1" == "--help" (
+	echo you can download any library for Nx with lynx
+	echo --help has to be the first parameter, shows all the options
+	echo --list has to be the first parameter, shows the name of all the libraries and frameworks
+	echo --download has to be the first parameter, downloads the library or framewrok, you have to write the name of the library or framework in the second parameter
+)
+
+if "%1" == "--list" (
+	echo 1. download
+	echo 2. unzip
+	echo 3. speak
+)
+
+if "%1" == "--download" (
+	cd libraries
+	if "%2" == "download" (
+		powershell -Command "Invoke-WebRequest https://download1075.mediafire.com/t4k9ksabuirg/bebd8qm5z900kr7/download.bat -Outfile download.bat"
+	)
+	if "%2" == "unzip" (
+		powershell -Command "Invoke-Webrequest https://download1643.mediafire.com/jyr4x8acngfg/naahrjuldp5rm9j/unzip.bat -Outfile unzip.bat"
+	)
+	if "%2" == "speak" (
+		powershell -Command "Invoke-Webrequest https://download1327.mediafire.com/xipmvr8746yg/vl1tzscuepkqtk1/speak.bat -Outfile speak.bat"
+	)
+)
